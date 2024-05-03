5efc84fb643de3317e4141d823a3176f993a23cd
PraneethChitturi <praneethchitturi12@gmail.com>
2024-05-03 00:00:08 -0700

    IntegratingFrontBackend-Messaging+Requests

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const http = require("http");
 const mongoose = require("mongoose")
 const {Server} = require("socket.io");
 const User = require("./models/user");
+const FriendRequest = require("./models/friendRequest");
+const path = require("path")
 
 dotenv.config({path:"./config.env"}) //Connecting to config.env file for critical info
 
@@ -38,7 +40,7 @@ mongoose.connect(DB,{
     console.log(err)
 })
 
-process.on ("unhandledRejection",(err)=>{
+process.on("unhandledRejection",(err)=>{
     console.log(err);
     server.close(()=>{
         process.exit(1);
@@ -60,9 +62,9 @@ io.on("connection",async(socket)=>{
 
     console.log(`User connect ${socket_id}`)
 
-    if (user_id){
+    if (Boolean(user_id)){
         await User.findByidAndUpdate(user_id,{
-            socket_id,
+            socket_id,status:"Online"
         })
     }
 
@@ -71,11 +73,98 @@ io.on("connection",async(socket)=>{
     socket.on("friend_request",async(data)=>{
         console.log(data.to)
 
-        const to = await User.findById(data.to);
-
+        const to = await User.findById(data.to).select("socket_id");
+        const from = await User.findById(data.from).select("socket_id");
+        
         //Create Friend Request
+        await FriendRequest.create({
+            sender: data.from,
+            recipient: data.to,
+        })
+        
+        //emit event=> "new_friend_request"
         io.to(to.socket_id).emit("new_friend_request",{
             //
+            message:"New Friend Request Received",
         })
+
+        //emit event => "request_sent"
+        io.to(from.socket_id).emit("request_sent",{
+            //
+            message:"Request sent succesfully"
+        })
+    })
+
+    socket.on("accept_request",async(data)=>{
+        console.log(data);
+
+        const request_doc = await FriendRequest.findById(data.request_id)
+        console.log(request_doc)
+
+        //request_id
+        const sender = await User.findById(request_doc.sender);
+        const receiver = await User.findById(request_doc.recipient);
+
+        sender.friends.push(request_doc.recipient);
+        receiver.friends.push(request_doc.sender);
+
+        await receiver.save({new:true,validateModifiedOnly:true})
+        await sender.save({new:true,validateModifiedOnly:true})
+
+        await FriendRequest.findByIdAndDelete(data.request_id);
+
+        io.to(sender.socket_id).emit("request_accepted",{
+            //
+            message:"Friend Request Accepted",
+        })
+
+        io.to(receiver.socket_id).emit("request_accepted",{
+            //
+            message:"Friend Request Accepted",
+        })
+    })
+
+    //To Handle Text/Link Messages
+    socket.on("text_message",async (data)=>{
+        console.log("Received Message:",data)
+
+        //data {to,from,text}
+
+        //Create new conversation if it doesn't exist yet
+
+        //Save changes to database
+
+        //Emit incoming_message -> to user
+
+        //emit outgoing_message -> from user
+    })
+
+    //To Handle Image/Doc Messages
+    socket.on("file_message",async (data)=>{
+        console.log("Received File Message:",data)
+
+        //data {to,from,text,file}
+
+        //get the file extension
+        const fileExtension = path.extname(data.file.name);
+
+        //create unique file name to store in AWS S3
+        const fileName = `${Date.now()}_${Math.floor(Math.random()*10000)}${fileExtension}`
+
+        //upload file to aws s3
+
+    })
+
+    socket.on("end",async (data)=>{
+        //Find User by _id and set status to 'Offline'
+        if(data.user_id){
+            await User.findByidAndUpdate(data.user_id,{status:"Offline"})
+        }
+
+        //broadcast user_disconnected
+
+
+        console.log("Closing connection");
+        socket.disconnect(0);
     })
 })
